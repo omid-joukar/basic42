e93c9a32defeb9959e59d92850a5f9fd528bbf2b
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-09 15:22:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/9/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner  scanner = new Scanner(System.in);
+        System.out.println("Please enter your password");
+        String password = scanner.next();
+        System.out.println(password);
+    }
+}
